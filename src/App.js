beaece4cad14e98a1411d1307def9d5cb3a213f5
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-12 09:06:07 -0500

    Create and link weather component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="container">
+        <h1>hi from weather</h1>
+      </div>
+    </div>
+  );
+}
