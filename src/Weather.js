9d720482883cd78f65fe47a08a70e9ba1496c985
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-12 12:49:58 -0500

    Add bootstrap and axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./index.css";
+import axios from "axios";
 
 export default function Weather() {
+  const apiKey = "fb8b95424c106907f53c4fc0092c4971";
+  let city = "New York";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   return (
     <div className="Weather">
       <div className="container">
-        <h1>hi from weather</h1>
         <div className="row search-section">
           <form className="search-bar row" id="search">
             <div className="search-title col-6">Search</div>
@@ -33,6 +36,28 @@ export default function Weather() {
               </button>
             </div>
           </form>
+
+          <div className="main shadow-lg p-3 mb-5 bg-body rounded">
+            <div className="row col-12">
+              <div className="currently-primary col-7">
+                <p className="currently-temp">51°F</p>
+                <p className="currently-city" id="currentlyCity">
+                  {city}
+                </p>
+                <p className="currently-country">US</p>
+              </div>
+              <div className="currently-sup-info col-5">
+                <i className="fas fa-sun weather-icon"></i>
+
+                <div className="current-desc">Partly Cloudy</div>
+
+                <div className="wind">Wind</div>
+                <div className="humidity">Humidity</div>
+              </div>
+
+              <hr />
+            </div>
+          </div>
         </div>
       </div>
     </div>
