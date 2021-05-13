5226fef671eb6069738669ee61a4e88ad89251be
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-13 09:54:57 -0500

    Add default city as a prop

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -84,9 +84,9 @@ export default function Weather() {
     );
   } else {
     const apiKey = "fb8b95424c106907f53c4fc0092c4971";
-    let city = "New York";
+
     let units = "imperial";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
