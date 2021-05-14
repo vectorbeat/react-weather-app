6e3e35311d01e31f15de8dffa0a8c7093bfe1835
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-14 08:48:30 -0500

    Create unit useState in Weather Temp

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <p className="currently-temp">
-        {Math.round(props.farenheit)}
-        <p className="weather-unit">°F</p>
-      </p>
-    </div>
-  );
+  const [unit, setUnit] = useState("farenheit");
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function showFarenheit(event) {
+    event.preventDefault();
+    setUnit("farenheit");
+  }
+  if (unit === "farenheit") {
+    return (
+      <div className="WeatherTemperature">
+        <p className="currently-temp">
+          {Math.round(props.farenheit)}
+          <p className="weather-unit">
+            °F |{" "}
+            <a href="/" onClick={showCelsius}>
+              °C
+            </a>
+          </p>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <p className="currently-temp">
+          {Math.round(props.farenheit)}
+          <p className="weather-unit">
+            <a href="/" onClick={showFarenheit}>
+              °F{" "}
+            </a>
+            | <strong>°C</strong>
+          </p>
+        </p>
+      </div>
+    );
+  }
 }
